5a82c0b6956b10bfd5551a4dcaee5dc5715c05e0
Shota Kudo <in-the-cloud.950@hotmail.co.jp>
2025-02-06 12:10:53 +0900

    some rename

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Change Log
 
-All notable changes to the "extension-sample" extension will be documented in this file.
+All notable changes to the "MQL5 on Linux" extension will be documented in this file.
 
 Check [Keep a Changelog](http://keepachangelog.com/) for recommendations on how to structure this file.
 
--- src/extension.ts
+++ src/extension.ts
@@ -7,21 +7,17 @@ import * as vscode from "vscode";
 export function activate(context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
-  console.log(
-    'Congratulations, your extension "extension-sample" is now active!',
-  );
+  console.log('Congratulations, your extension "MQL5 on Linux" is now active!');
 
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
   const disposable = vscode.commands.registerCommand(
-    "extension-sample.helloWorld",
+    "mql5-linux.helloWorld",
     () => {
       // The code you place here will be executed every time your command is executed
       // Display a message box to the user
-      vscode.window.showInformationMessage(
-        "Hello World from extension-sample!",
-      );
+      vscode.window.showInformationMessage("Hello World from MQL5 on Linux!");
     },
   );
 
